2b11edf3cb62c79d2396a1a1c0e17d241e4ae436
2nthony <ant.hu@outlook.com>
2022-05-17 17:18:17 +0800

    feat: markdown preview

---
+++ src/components/MarkdownPreview.tsx
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+import { marked } from "marked";
+import React from "react";
+import sanitizeHtml from "sanitize-html";
+import Prism from "prismjs";
+import "github-markdown-css";
+import "prismjs/themes/prism.css";
+
+marked.setOptions({
+  highlight(code, lang) {
+    if (!lang) {
+      return code;
+    }
+
+    const language = Prism.languages[lang] ?? Prism.languages.markup;
+    return Prism.highlight(code, language, lang);
+  }
+});
+
+const MarkdownContent = styled.div``;
+
+type Props = {
+  content?: string;
+  allowedTags?: string[];
+  style?: React.CSSProperties;
+};
+
+function MarkdownPreview(props: Props = {}) {
+  const {
+    content = "",
+    allowedTags = sanitizeHtml.defaults.allowedTags.concat([
+      "img",
+      "iframe",
+      "br"
+    ]),
+    ...restProps
+  } = props;
+
+  const html = marked.parse(content);
+
+  const cleanHtml = sanitizeHtml(html, {
+    allowedTags,
+    allowedAttributes: {
+      img: ["src", "size", "width", "height"],
+      iframe: ["src", "width", "height"],
+      a: ["href", "rel", "target"],
+      "*": ["class"],
+      td: ["align"],
+      th: ["align"]
+    }
+  });
+
+  return (
+    <MarkdownContent {...restProps} className="markdown-preview">
+      <div
+        className="markdown-body"
+        dangerouslySetInnerHTML={{ __html: cleanHtml }}
+      />
+    </MarkdownContent>
+  );
+}
+
+export default MarkdownPreview;
--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import type { TextController } from "./types/CommandOptions";
 import { TextAreaTextController } from "./text/textarea-text-controller";
 import * as textHelpers from "./helpers/textHelpers";
 import * as listHelpers from "./helpers/listHelpers";
-import * as headerHelpers from "./helpers/headerHelpers"
+import * as headerHelpers from "./helpers/headerHelpers";
 import { codeCommand } from "./commands/markdown-commands/codeCommand";
 import { useTextAreaMarkdownEditor } from "./hooks/use-markdown-editor";
 import { codeBlockCommand } from "./commands/markdown-commands/codeBlockCommand";
@@ -24,11 +24,24 @@ import { headingLevel4Command } from "./commands/markdown-commands/headingLevel4
 import { headingLevel5Command } from "./commands/markdown-commands/headingLevel5Command";
 import { headingLevel6Command } from "./commands/markdown-commands/headingLevel6Command";
 import * as React from "react";
-import {Box, ChakraProvider, HStack, Textarea} from "@chakra-ui/react";
-import {ToolbarButton} from "../demo/toolbar-button";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBold, faCode, faHeading, faItalic} from "@fortawesome/free-solid-svg-icons";
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  HStack,
+  Spacer,
+  Textarea
+} from "@chakra-ui/react";
+import { ToolbarButton } from "../demo/toolbar-button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBold,
+  faCode,
+  faHeading,
+  faItalic
+} from "@fortawesome/free-solid-svg-icons";
 import ReactDOM from "react-dom";
+import MarkdownPreview from "./components/MarkdownPreview";
 
 export {
   // helpers
@@ -73,47 +86,64 @@ export const Editor: React.FunctionComponent<DemoProps> = () => {
     }
   });
 
+  const [mdString, setMdString] = React.useState("");
+  const [editStatus, setEditStatus] = React.useState<"write" | "preview">(
+    "write"
+  );
+  const isPreview = React.useMemo(() => {
+    return editStatus === "preview";
+  }, [editStatus]);
+
   return (
     <ChakraProvider>
       <Box p={3}>
-    <HStack py={2}>
-    <ToolbarButton
-      onClick={async () => {
-    await commandController.executeCommand("bold");
-  }}
->
-  <FontAwesomeIcon icon={faBold} />
-  </ToolbarButton>
-  <ToolbarButton
-  onClick={async () => {
-    await commandController.executeCommand("italic");
-  }}
->
-  <FontAwesomeIcon icon={faItalic} />
-  </ToolbarButton>
-  <ToolbarButton
-  onClick={async () => {
-    await commandController.executeCommand("code");
-  }}
->
-  <FontAwesomeIcon icon={faCode} />
-  </ToolbarButton>
-  <ToolbarButton
-  onClick={async () => {
-    await commandController.executeCommand("headingLevel1");
-  }}
->
-  <FontAwesomeIcon icon={faHeading} />
-  </ToolbarButton>
-  </HStack>
-  <Textarea
-  ref={ref}
-  placeholder="osn markdown editor"
-  fontFamily={"monospace"}
-  />
-  </Box>
-  </ChakraProvider>
-);
+        <HStack py={2}>
+          <Button onClick={() => setEditStatus("write")}>Write</Button>
+          <Button onClick={() => setEditStatus("preview")}>Preview</Button>
+          <Spacer />
+          <ToolbarButton
+            onClick={async () => {
+              await commandController.executeCommand("bold");
+            }}
+          >
+            <FontAwesomeIcon icon={faBold} />
+          </ToolbarButton>
+          <ToolbarButton
+            onClick={async () => {
+              await commandController.executeCommand("italic");
+            }}
+          >
+            <FontAwesomeIcon icon={faItalic} />
+          </ToolbarButton>
+          <ToolbarButton
+            onClick={async () => {
+              await commandController.executeCommand("code");
+            }}
+          >
+            <FontAwesomeIcon icon={faCode} />
+          </ToolbarButton>
+          <ToolbarButton
+            onClick={async () => {
+              await commandController.executeCommand("headingLevel1");
+            }}
+          >
+            <FontAwesomeIcon icon={faHeading} />
+          </ToolbarButton>
+        </HStack>
+
+        <Textarea
+          ref={ref}
+          value={mdString}
+          style={{ display: !isPreview ? "block" : "none" }}
+          onChange={e => setMdString(e.target.value)}
+          placeholder="osn markdown editor"
+          fontFamily={"monospace"}
+        />
+
+        {isPreview && <MarkdownPreview content={mdString} />}
+      </Box>
+    </ChakraProvider>
+  );
 };
 
 ReactDOM.render(<Editor />, document.getElementById("root"));
