8e3556d0f8b380243afe0342ee9a8fa908378bfb
2nthony <ant.hu@outlook.com>
2022-06-22 14:36:05 +0800

    fix: esc key to close mention popup

--- src/util/eventHandlers.tsx
+++ src/util/eventHandlers.tsx
@@ -36,7 +36,7 @@ export function getHandlers({
         event.preventDefault();
         handleSuggestionSelected(focusIndex);
       }
-      if (event.key === "Backspace") {
+      if (event.key === "Escape") {
         setShowSuggestion(false);
       }
     }
