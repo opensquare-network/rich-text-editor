9ee2b3aeadae7486966e6fc3e80a5257505aaaa6
2nthony <ant.hu@outlook.com>
2022-06-27 14:44:17 +0800

    fix: mention key bindings

--- src/quillModules/mention.ts
+++ src/quillModules/mention.ts
@@ -9,11 +9,11 @@ import {
 import "./blots/mention";
 
 const Keys = {
-  TAB: 9,
-  ENTER: 13,
-  ESCAPE: 27,
-  UP: 38,
-  DOWN: 40
+  TAB: "Tab",
+  ENTER: "Enter",
+  ESCAPE: "Escape",
+  UP: "ArrowUp",
+  DOWN: "ArrowDown"
 };
 
 class Mention {
